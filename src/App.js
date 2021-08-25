f915470d476fa5fe67d5077a7854c472d1393d24
Guilherme Girardi <girardi.gui@icloud.com>
2021-08-25 11:18:59 +0100

    Edit routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
-import { UnauthenticatedApp } from './app/Unauthenticated-App'
-import { AuthenticatedApp } from './app/Authenticated-App'
+import {UnauthenticatedApp} from './app/Unauthenticated-App'
+import {AuthenticatedApp} from './app/Authenticated-App'
 
 function App() {
   return (
-    <Router>  
-      <Switch>
-        <Route exact path='/' component={UnauthenticatedApp}/>
-        <Route exact path='/dashboard' component={AuthenticatedApp}/>
-      </Switch>
-    </Router>
+    <AuthenticatedApp />
   )
 }
 
--- src/app/Authenticated-App/index.js
+++ src/app/Authenticated-App/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import './styles.scss'
 import {Header} from '../../components/Header'
@@ -13,12 +14,25 @@ function AuthenticatedApp() {
   return (
     <section className="dashboard">
       <Sidebar />
-      
       <div className="dashboard__header-container">
         <Header />
-        <Movies />
+        <AppRoutes />
       </div>
     </section>
+  )
+}
+
+function AppRoutes() {
+  return (
+    <Router>  
+      <Switch>
+        <Route exact path='/dashboard' component={Home} />
+        <Route exact path='/books' component={Books} />
+        <Route exact path='/movies' component={Movies} />
+        <Route exact path='/characters' component={Characters} />
+        <Route exact path='/quotes' component={Quotes} />
+      </Switch>
+    </Router>
   )
 }
 
