9fcaa54d60ab1d9df97577adfdb1677bc20f2d13
Guilherme Girardi <girardi.gui@icloud.com>
2021-09-23 10:36:28 +0100

    Edit testing

--- src/components/Login/index-new.test.js
+++ src/components/Login/index-new.test.js
@@ -1,36 +1,26 @@
 import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import {build, fake} from '@jackfranklin/test-data-bot'
+
+// import {renderHook, act} from '@testing-library/react-hooks'
+// import {build, fake} from '@jackfranklin/test-data-bot'
 
 import {AppProviders} from '../../context'
-import {useAuth} from '../../context/auth-context'
-
 import {Login} from './index-new'
 
-const buildLoginForm = build({
-  fields: {
-    username: fake(f => f.internet.userName()),
-    password: fake(f => f.internet.password()),
-  },
-})
+// import {useAuth} from '../../context/auth-context'
+// import * as auth from '../../provider/auth-provider'
 
 test('it should login the user', async () => {
   // arrange
-  const handleSubmit = jest.fn()
   render(<Login />, {wrapper: AppProviders})
-  const {username, password} = buildLoginForm()
-  let spyOnLogin = jest.spyOn(useAuth, 'login');
 
   // act
   await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
 
-  // assert
-  userEvent.type(screen.getByLabelText(/username/i), username)
-  userEvent.type(screen.getByLabelText(/password/i), password)
-  userEvent.click(screen.getByRole('button', {name: /submit\-btn/i}))
-
-  expect(spyOnLogin).toHaveBeenCalledTimes(1);
+  userEvent.type(screen.getByLabelText(/username/i), 'gui')
+  userEvent.type(screen.getByLabelText(/password/i), 'xxx')
+  userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
   screen.debug()
 })
